4b8ed01759445b5b737be4766fbcedaea007acc7
RohanWingherry <rohan.wingherry@gmail.com>
2025-02-18 21:06:02 +0530

    changes

--- js/homepage.js
+++ js/homepage.js
@@ -95,120 +95,120 @@ document.getElementById("yuktha-image-section").addEventListener("click", handle
 document.getElementById("wingherry-image-section").addEventListener("click", handleImageClick);
 
 // Get in Touch
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("getintouchbtn");
-var span = document.getElementsByClassName("close")[0];
+// var modal = document.getElementById("myModal");
+// var btn = document.getElementById("getintouchbtn");
+// var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-span.onclick = function() {
-  modal.style.display = "none";
-}
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-function checkWordLimit() {
-  var wordCount = document.getElementById("enquireAbout").value.split(/\s+/).length;
-  if (wordCount === 1 && document.getElementById("enquireAbout").value === "") {
-      wordCount = 0;
-  }
-  document.getElementById("wordCount").textContent = "Word count: " + wordCount + "/250";
-}
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+// function checkWordLimit() {
+//   var wordCount = document.getElementById("enquireAbout").value.split(/\s+/).length;
+//   if (wordCount === 1 && document.getElementById("enquireAbout").value === "") {
+//       wordCount = 0;
+//   }
+//   document.getElementById("wordCount").textContent = "Word count: " + wordCount + "/250";
+// }
 
-// Function to validate form
-document.getElementById('firstName').addEventListener('input', function(event) {
-    event.target.value = event.target.value.replace(/[^a-zA-Z]/g, '');
-});
-document.getElementById('lastName').addEventListener('input', function(event) {
-  event.target.value = event.target.value.replace(/[^a-zA-Z]/g, '');
-});
-document.getElementById('organisation').addEventListener('input', function(event) {
-  let value = event.target.value;
+// // Function to validate form
+// document.getElementById('firstName').addEventListener('input', function(event) {
+//     event.target.value = event.target.value.replace(/[^a-zA-Z]/g, '');
+// });
+// document.getElementById('lastName').addEventListener('input', function(event) {
+//   event.target.value = event.target.value.replace(/[^a-zA-Z]/g, '');
+// });
+// document.getElementById('organisation').addEventListener('input', function(event) {
+//   let value = event.target.value;
 
-  value = value.replace(/[^a-zA-Z ]/g, '');
-  value = value.replace(/^\s+/, '');
+//   value = value.replace(/[^a-zA-Z ]/g, '');
+//   value = value.replace(/^\s+/, '');
 
-  event.target.value = value;
-});
-document.getElementById('title').addEventListener('input', function(event) {
-  let value = event.target.value;
+//   event.target.value = value;
+// });
+// document.getElementById('title').addEventListener('input', function(event) {
+//   let value = event.target.value;
 
-  value = value.replace(/[^a-zA-Z ]/g, '');
-  value = value.replace(/^\s+/, '');
+//   value = value.replace(/[^a-zA-Z ]/g, '');
+//   value = value.replace(/^\s+/, '');
 
-  event.target.value = value;
-});
-document.getElementById('businessEmail').addEventListener('input', function(event) {
-  event.target.value = event.target.value.replace(/[^a-zA-Z0-9@.-_]/g, '');
-});
-document.getElementById('enquireAbout').addEventListener('input', function(event) {
-  let value = event.target.value;
-  if (value.length === 1 && !/[a-zA-Z]/.test(value.charAt(0))) {
-    event.target.value = '';
-  }
-});
+//   event.target.value = value;
+// });
+// document.getElementById('businessEmail').addEventListener('input', function(event) {
+//   event.target.value = event.target.value.replace(/[^a-zA-Z0-9@.-_]/g, '');
+// });
+// document.getElementById('enquireAbout').addEventListener('input', function(event) {
+//   let value = event.target.value;
+//   if (value.length === 1 && !/[a-zA-Z]/.test(value.charAt(0))) {
+//     event.target.value = '';
+//   }
+// });
 
-function validateForm() {
-  var firstName = document.getElementById("firstName").value.trim();
-  var lastName = document.getElementById("lastName").value.trim();
-  var organisation = document.getElementById("organisation").value.trim();
-  var title = document.getElementById("title").value.trim();
-  var businessEmail = document.getElementById("businessEmail").value.trim();
-  var enquireAbout = document.getElementById("enquireAbout").value.trim();
+// function validateForm() {
+//   var firstName = document.getElementById("firstName").value.trim();
+//   var lastName = document.getElementById("lastName").value.trim();
+//   var organisation = document.getElementById("organisation").value.trim();
+//   var title = document.getElementById("title").value.trim();
+//   var businessEmail = document.getElementById("businessEmail").value.trim();
+//   var enquireAbout = document.getElementById("enquireAbout").value.trim();
 
-  removeErrorBorders();
+//   removeErrorBorders();
 
-  if (!firstName || !lastName || !organisation || !title || !businessEmail || !enquireAbout) {
-      alert("Please fill out all required fields.");
-      if (!firstName) {
-          document.getElementById("firstName").classList.add("error");
-      }
-      if (!lastName) {
-          document.getElementById("lastName").classList.add("error");
-      }
-      if (!organisation) {
-          document.getElementById("organisation").classList.add("error");
-      }
-      if (!title) {
-          document.getElementById("title").classList.add("error");
-      }
-      if (!businessEmail) {
-          document.getElementById("businessEmail").classList.add("error");
-      }
-      if (!enquireAbout) {
-          document.getElementById("enquireAbout").classList.add("error");
-      }
-  } else {
-      alert("Form submitted successfully!");
-      modal.style.display = "none";
-      document.getElementById("contactForm").reset();
-  }
-}
-function removeErrorBorders() {
-  var fields = document.querySelectorAll('input, textarea');
-  fields.forEach(function(field) {
-      field.classList.remove("error");
-  });
-}
+//   if (!firstName || !lastName || !organisation || !title || !businessEmail || !enquireAbout) {
+//       alert("Please fill out all required fields.");
+//       if (!firstName) {
+//           document.getElementById("firstName").classList.add("error");
+//       }
+//       if (!lastName) {
+//           document.getElementById("lastName").classList.add("error");
+//       }
+//       if (!organisation) {
+//           document.getElementById("organisation").classList.add("error");
+//       }
+//       if (!title) {
+//           document.getElementById("title").classList.add("error");
+//       }
+//       if (!businessEmail) {
+//           document.getElementById("businessEmail").classList.add("error");
+//       }
+//       if (!enquireAbout) {
+//           document.getElementById("enquireAbout").classList.add("error");
+//       }
+//   } else {
+//       alert("Form submitted successfully!");
+//       modal.style.display = "none";
+//       document.getElementById("contactForm").reset();
+//   }
+// }
+// function removeErrorBorders() {
+//   var fields = document.querySelectorAll('input, textarea');
+//   fields.forEach(function(field) {
+//       field.classList.remove("error");
+//   });
+// }
 
-// Text area
-function checkWordLimit() {
-  const textarea = document.getElementById('enquireAbout');
-  const wordCountDisplay = document.getElementById('wordCount');
-  const text = textarea.value.trim();
-  const wordCount = text ? text.split(/\s+/).length : 0;
+// // Text area
+// function checkWordLimit() {
+//   const textarea = document.getElementById('enquireAbout');
+//   const wordCountDisplay = document.getElementById('wordCount');
+//   const text = textarea.value.trim();
+//   const wordCount = text ? text.split(/\s+/).length : 0;
   
-  wordCountDisplay.textContent = `Word count: ${wordCount}/250`;
+//   wordCountDisplay.textContent = `Word count: ${wordCount}/250`;
   
-  if (wordCount > 250) {
-      const words = text.split(/\s+/).slice(0, 250).join(' ');
-      textarea.value = words;
-      wordCountDisplay.textContent = `Word count: 250/250`;
-  }
-}
+//   if (wordCount > 250) {
+//       const words = text.split(/\s+/).slice(0, 250).join(' ');
+//       textarea.value = words;
+//       wordCountDisplay.textContent = `Word count: 250/250`;
+//   }
+// }
 
 // Spinner
 window.addEventListener("load", () => {
